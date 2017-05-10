d0cb96e691eb72cf28c90b3201c1bf940800c39e
Jori <joriguzman@yahoo.com>
2017-05-10 22:12:25 +0800

    Refactor App tests

--- src/App.test.js
+++ src/App.test.js
@@ -9,32 +9,26 @@ import TopicList from './TopicList';
 import jsonData from './database.json';
 
 describe('App', () => {
+  const app = shallow(<App jsonObj={jsonData} />);
+
   it('renders without exploding', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App jsonObj={jsonData} />, div);
   });
 
   it('renders Title component', () => {
-    const app = shallow(<App jsonObj={jsonData} />);
-
     expect(app.find(Title).length).toBe(1);
   });
 
   it('renders UserProfile component', () => {
-    const app = shallow(<App jsonObj={jsonData} />);
-
     expect(app.find(UserProfile).length).toBe(1);
   });
 
   it('renders SubmitTopicForm component', () => {
-    const app = shallow(<App jsonObj={jsonData} />);
-
     expect(app.find(SubmitTopicForm).length).toBe(1);
   });
 
   it('renders TopicList component', () => {
-    const app = shallow(<App jsonObj={jsonData} />);
-
     expect(app.find(TopicList).length).toBe(1);
   });
 });

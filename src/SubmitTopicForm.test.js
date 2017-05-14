598efd93b3e34c02f2be20e4c0ab6d9429b72dc9
Jori <joriguzman@yahoo.com>
2017-05-15 03:49:44 +0800

    Update SubmitTopic tests

--- src/SubmitTopicForm.test.js
+++ src/SubmitTopicForm.test.js
@@ -18,29 +18,29 @@ describe('SubmitTopicForm', () => {
     });
 
     it('enables submit button if there is a topic', () => {
-        // const submitForm = shallow(<SubmitTopicForm />);
-        // const input = submitForm.find('textarea');
-        // input.simulate('change', { target: { value: 'Changed' } });
+        const submitForm = shallow(<SubmitTopicForm />);
+        const textarea = submitForm.find('textarea');
 
-        // expect(submitForm.find('button').prop('disabled')).toBe(true);
+        textarea.simulate('change', { target: { value: 'Changed' } });
+
+        expect(submitForm.find('button').prop('disabled')).toBe(false);
     });
 
-    it('calls handleTopicInputChange when input is changed', () => {
-        // const handleTopicInputChange = jest.fn();
-        // const submitForm = shallow(
-        //     <SubmitTopicForm onTopicInputChange={handleTopicInputChange} />);
+    it('sets state when input is changed', () => {
+        const submitForm = shallow(<SubmitTopicForm />);
+        const textarea = submitForm.find('textarea');
 
-        // submitForm.find('textarea').simulate('change');
+        textarea.simulate('change', { target: { value: 'Changed' } });
 
-        // expect(handleTopicInputChange).toBeCalled();
+        expect(submitForm.state().newTopic).toBe('Changed');
     });
 
     it('calls handleSubmit when input is changed', () => {
-        // const handleSubmit = jest.fn();
-        // const submitForm = shallow(<SubmitTopicForm onSubmit={handleSubmit} />);
+        const handleSubmit = jest.fn();
+        const submitForm = shallow(<SubmitTopicForm handleSubmit={handleSubmit} />);
 
-        // submitForm.find('form').simulate('submit');
+        submitForm.find('form').simulate('submit');
 
-        // expect(handleSubmit).toBeCalled();
+        expect(handleSubmit).toBeCalled();
     });
 });

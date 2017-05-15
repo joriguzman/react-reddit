d2209ffa518546575f7bb59017df29d9bae12779
Jori <joriguzman@yahoo.com>
2017-05-15 18:05:53 +0800

    Fix App tests

--- src/App.test.js
+++ src/App.test.js
@@ -30,34 +30,54 @@ describe('App', () => {
     ];
 
     it('renders without exploding', () => {
-        const api = jest.mock(); // TODO fix test warning
+        const api = {
+            changeDisplay() {
+                return jest.mock();
+            }
+        };
         const div = document.createElement('div');
         ReactDOM.render(<App topics={topics} api={api} />, div);
     });
 
     it('renders Title component', () => {
-        const api = jest.mock();
+        const api = {
+            changeDisplay() {
+                return jest.mock();
+            }
+        };
         const app = shallow(<App topics={topics} api={api} />);
 
         expect(app.find(Title).length).toBe(1);
     });
 
     it('renders UserProfile component', () => {
-        const api = jest.mock();
+        const api = {
+            changeDisplay() {
+                return jest.mock();
+            }
+        };
         const app = shallow(<App topics={topics} api={api} />);
 
         expect(app.find(UserProfile).length).toBe(1);
     });
 
     it('renders SubmitTopicForm component', () => {
-        const api = jest.mock();
+        const api = {
+            changeDisplay() {
+                return jest.mock();
+            }
+        };
         const app = shallow(<App topics={topics} api={api} />);
 
         expect(app.find(SubmitTopicForm).length).toBe(1);
     });
 
     it('renders TopicList component', () => {
-        const api = jest.mock();
+        const api = {
+            changeDisplay() {
+                return jest.mock();
+            }
+        };
         const app = shallow(<App topics={topics} api={api} />);
 
         expect(app.find(TopicList).length).toBe(1);
